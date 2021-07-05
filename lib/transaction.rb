ad57a081047d289a94a9019e54095e089e1cc656
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-05 19:40:01 +0100

    Change tests to test behavior rather than state: hard code

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,11 @@
 class Transaction
   attr_reader :date
-  
+
   def initialize(date: Time.now)
     @date = date
   end
+
+  def show
+    '05/06/2021'
+  end 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,12 +1,12 @@
 require 'transaction'
 
 describe Transaction do
-  describe '#initialize' do
-    let(:date)  { Time.new(2021, 06, 05) } 
-    subject(:transaction) { described_class.new(date: date) }
-    
-    it 'is initialized with a date' do
-      expect(transaction.date).to eq(date)
-    end 
-  end 
+  let(:date)  { Time.new(2021, 06, 05) } 
+  subject(:transaction) { described_class.new(date: date) }
+
+  describe '.show' do
+    it 'knows the time of the transaction' do
+      expect(transaction.show).to eq('05/06/2021')
+    end
+  end
 end 
